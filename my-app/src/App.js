fa90a0e916b4b9421f5d13acd6ceb25303c621b9
maely brandão <56039385+mxlysb@users.noreply.github.com>
2023-10-23 23:40:26 -0300

    Building Recommendation Logic

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import $ from 'jquery';
+import PromptComponent from './PromptComponent';
 
 class App extends React.Component {
   constructor(props) {
@@ -23,28 +24,67 @@ class App extends React.Component {
   }
 
 
-  topTracksLorde = () =>{
+  getRecommendation = (artistsId) =>{
+    console.log(artistsId)
+    const artists = artistsId.join(",")
     $.ajax({
       method: "GET",
       dataType: "json",
-      url:"https://api.spotify.com/v1/artists/163tK9Wjr9P9DmM0AVK7lm/top-tracks?market=BR",
+      url:`https://api.spotify.com/v1/recommendations?limit=10&market=BR&seed_artists=${artists}`,
       headers: {
         Authorization: `Bearer ${this.token}`
       }
     })
     .then(dados => {
-      console.log(dados.tracks[0].name)
+      for (const track of dados.tracks) {
+        console.log(track.name)
+      }
     })
     .catch(error => {
       console.error(error);
     });
   }
 
+  handleValueFromPrompt = (value) => {
+    return new Promise(async (resolve, reject) => {
+      const artists = value.split(',');
+      const artistsId = [];
+  
+      for (const artist of artists) {
+        try {
+          const response = await fetch(`https://api.spotify.com/v1/search?q=${encodeURIComponent(artist)}&type=artist`, {
+            method: 'GET',
+            headers: {
+              'Authorization': `Bearer ${this.token}`,
+            },
+          });
+  
+          const data = await response.json();
+  
+          if (data.artists && data.artists.items.length > 0) {
+            artistsId.push(data.artists.items[0].id);
+            console.log(artistsId);
+          } else {
+            console.log(`Nenhum artista encontrado para "${artist}"`);
+          }
+        } catch (error) {
+          console.error('Erro ao pesquisar o artista:', error);
+          reject(error);
+          return;
+        }
+      }
+  
+      resolve(artistsId);
+      this.getRecommendation(artistsId)
+    });
+  };
+  
+
   render() {
     return (
       <div className="App">
         <button><a href="http://localhost:8888">Logar com Spotify</a></button>
-        <button onClick={this.topTracksLorde}>Buscar top tracks da Lorde</button>
+        <PromptComponent onValueSubmit={this.handleValueFromPrompt} />
       </div>
     );
   }
---
+++ my-app/src/PromptComponent.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+
+class PromptComponent extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      inputValue: '',
+      isPromptVisible: false,
+    };
+  }
+
+  handlePromptOpen = () => {
+    this.setState({ isPromptVisible: true });
+  };
+
+  handlePromptClose = () => {
+    this.setState({ isPromptVisible: false });
+  };
+
+  handleInputChange = (e) => {
+    this.setState({ inputValue: e.target.value });
+  };
+
+  handlePromptSubmit = () => {
+    const inputValue = this.state.inputValue;
+    this.props.onValueSubmit(inputValue); // Chama a função de retorno de chamada com o valor.
+    this.handlePromptClose();
+  };
+  
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handlePromptOpen}>Digite artistas</button>
+        {this.state.isPromptVisible && (
+          <div className="prompt">
+            <input
+              type="text"
+              value={this.state.inputValue}
+              onChange={this.handleInputChange}
+            />
+            <button onClick={this.handlePromptSubmit}>Enviar</button>
+            <button onClick={this.handlePromptClose}>Cancelar</button>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default PromptComponent;
